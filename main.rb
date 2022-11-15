4036212d2181264ab02455acc911d7e1a537964b
Jessica Kerr <jessitron@honeycomb.io>
2022-11-15 01:46:33 +0000

    beginning

---
+++ main.rb
@@ -0,0 +1,25 @@
+print "hello!\n"
+
+# configure
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+OpenTelemetry::SDK.configure do |c|
+  print "Configuring open telemetry\n"
+end
+
+# now go
+
+tracer = OpenTelemetry.tracer_provider.tracer('paint')
+tracer.in_span("noodles", kind: :server, attributes: { "coolness" => 96 }) do |span|
+  # ... cool stuff
+end
+tracer.in_span("noodles", kind: :server, attributes: { "coolness" => 98 }) do |span|
+  # ... cool stuff
+end
+tracer.in_span("noodles", kind: :server, attributes: { "coolness" => 97 }) do |span|
+  # ... cool stuff
+end
+
+# maybe wait around for it to send?
+sleep 10
+print "Good bye\n"
